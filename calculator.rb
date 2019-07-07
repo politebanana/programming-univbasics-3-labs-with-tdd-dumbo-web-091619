a1f7c3f49b3ddecaa344a1a6d9fab644dd58d04b
Christian Zambrana <chrislajas@gmail.com>
2019-07-07 05:50:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,12 @@
 # Add your variables here
+first_number = get_variable_from_file
+expect(first_number).to be_an(Integer).or be_a(Float)
+second_number = get_variable_from_file
+expect(second_number).to be_an(Integer).or be_a(Float)
+expect(second_number).not_to equal(0)
+expect(sum).to eq(first_number+second_number)
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
